7c234d38d9a270c03966c6ae8c826c5f3622627d
TsitouhRanjafy <radohery09@gmail.com>
2025-03-26 13:43:31 +0100

    ensure LoginRouter returns 400 if no httpRequest is provided

--- test/login-router.spec.ts
+++ test/login-router.spec.ts
@@ -1,15 +1,21 @@
 import {describe, expect, test} from '@jest/globals';
 class LoginRouter {
     route(httpRequest: httpRequest): httpRequest {
-        const body: login = httpRequest.body
-        if (!body.email || !body.password){
+        if (!httpRequest){
             return {
-                body: body,
+                body: { email: '',password:''},
+                statusCode: 400
+            }
+        }
+        const { email,password } = httpRequest.body
+        if (!email || !password){
+            return {
+                body: { email,password },
                 statusCode: 400
             }
         }
         return {
-            body: body,
+            body: { email,password },
             statusCode: 200
         }
     }
@@ -50,6 +56,14 @@ describe('Login Router',() => {
             statusCode: 0
         } 
         const httpResponse: httpRequest = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+
+    test('Should return 400 if no httpRequest is provided',() => {
+        const sut = new LoginRouter()
+        const httpRequest: any = undefined; // eslint-disable-line 
+        const httpResponse: httpRequest = sut.route(httpRequest) // eslint-disable-line 
         expect(httpResponse.statusCode).toBe(400)
     })
 })
